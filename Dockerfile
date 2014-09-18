5f67ab39a7f082c9b718cd9e7e0d33cf109a742e
Dennis Kanbier <dennis@kanbier.net>
2014-09-18 19:13:14 +0000

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+FROM centos
+MAINTAINER Dennis Kanbier <dennis@kanbier.net>
+
+# Update base images.
+RUN yum distribution-synchronization -y
+RUN yum makecache
+
+# Install MySQL software 
+RUN yum -y -q install mysql mysql-server 
+
+# Cleanup
+RUN yum clean all
+
+# MySQL Config
+ADD ./mysql/my.cnf /etc/mysql/conf.d/my.cnf
+ADD ./zabbix/create/data.sql /tmp/create/data.sql
+ADD ./zabbix/create/images.sql /tmp/create/images.sql
+ADD ./zabbix/create/schema.sql /tmp/create/schema.sql
+
+# https://github.com/dotcloud/docker/issues/1240#issuecomment-21807183
+RUN echo "NETWORKING=yes" > /etc/sysconfig/network
+
+# Custom script to setup Zabbix schema and start mysqld_safe
+ADD ./scripts/start.sh /start.sh
+RUN chmod 755 /start.sh
+
+# Expose MySQL default port  
+EXPOSE 3306 
+
+# Use a volume for MySQL data
+VOLUME ["/var/lib/mysql"]
+
+# Sart the custom start script
+CMD ["/bin/bash", "/start.sh"]
---
+++ scripts/start.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ ! -f /mysql-configured ]; then 
+ 	/sbin/service mysqld restart 
+
+ 	/usr/bin/mysql_upgrade
+
+	 sleep 10s
+
+	 MYSQL_PASSWORD="mypassword"
+
+	 echo "mysql root and admin password: $MYSQL_PASSWORD"
+
+	 echo "$MYSQL_PASSWORD" > /mysql-root-pw.txt
+
+	 mysqladmin -uroot password $MYSQL_PASSWORD 
+
+	 mysql -uroot -p"$MYSQL_PASSWORD" -e "INSERT INTO mysql.user (Host,User,Password) VALUES('%','admin',PASSWORD('${MYSQL_PASSWORD}'));"
+
+	 mysql -uroot -p"$MYSQL_PASSWORD" -e "GRANT ALL ON *.* TO 'admin'@'%';"
+
+	 mysqladmin -uroot -p"$MYSQL_PASSWORD" create zabbix
+
+	 mysql -uroot -p"$MYSQL_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS zabbix CHARACTER SET utf8;"
+
+	 zabbix_mysql_v="/tmp"
+
+	 mysql -uroot -D zabbix -p"$MYSQL_PASSWORD" < "${zabbix_mysql_v}/create/schema.sql"
+	 mysql -uroot -D zabbix -p"$MYSQL_PASSWORD" < "${zabbix_mysql_v}/create/images.sql"
+	 mysql -uroot -D zabbix -p"$MYSQL_PASSWORD" < "${zabbix_mysql_v}/create/data.sql"
+
+	 mysql -uroot -p"$MYSQL_PASSWORD" -e "INSERT INTO mysql.user (Host,User,Password) VALUES('localhost','zabbix',PASSWORD('zabbix'));"
+
+	 mysql -uroot -p"$MYSQL_PASSWORD" -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON zabbix.* TO 'zabbix'@'%';"
+
+	 /sbin/service mysqld stop
+
+	 touch /mysql-configured
+fi
+
+mysqld_safe
